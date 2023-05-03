b96f22b1037d13dbd2825d843533cef8e4b0f5d2
rishabh1208 <rishabhsinghal1215@gmail.com>
2023-05-02 20:23:33 -0700

    minor changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log("Mongo DB Connected: ", conn.connection.host)
-    }
-    catch(err) {
-        console.log(err)
-        process.exit(1)
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Mongo DB Connected: ", conn.connection.host);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
