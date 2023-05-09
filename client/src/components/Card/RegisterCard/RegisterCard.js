1641d83a6bd2ed0f474333d62230f5f1b4d4bbaa
rishabh1208 <rishabhsinghal1215@gmail.com>
2023-05-08 18:12:06 -0700

    fixed login and register

--- client/src/components/Card/RegisterCard/RegisterCard.js
+++ client/src/components/Card/RegisterCard/RegisterCard.js
@@ -1,7 +1,34 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link , useNavigate} from "react-router-dom";
+import BASE_URL from "../../../utils/api";
 import "./RegisterCard.css";
+import axios from "axios";
 
 const RegisterCard = () => {
+  const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${BASE_URL}/api/auth/register`, {
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+      console.log(res.data); // log the response from the API
+      if (res.data.success == true) {
+        navigate('/account/login'); // Navigate to login component
+      }
+    } catch (err) {
+      console.error(err); // handle error
+    }
+  };
+
   return (
     <div className="register__card__container">
       <div className="register__card">
@@ -11,11 +38,21 @@ const RegisterCard = () => {
         <div className="register__inputs">
           <div className="fname__input__container reg__input__container">
             <label className="fname__label input__label">First name</label>
-            <input type="text" className="fname__input register__input" />
+            <input
+              type="text"
+              className="fname__input register__input"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
           <div className="lname__input__container reg__input__container">
             <label className="lname__label input__label">Last name</label>
-            <input type="text" className="lname__input register__input" />
+            <input
+              type="text"
+              className="lname__input register__input"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
           </div>
           <div className="email__input__container reg__input__container">
             <label className="email__label input__label">Email</label>
@@ -23,6 +60,8 @@ const RegisterCard = () => {
               type="email"
               className="email__input register__input"
               placeholder="example@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="password__input__container reg__input__container">
@@ -30,10 +69,14 @@ const RegisterCard = () => {
             <input
               type="password"
               className="password__input register__input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="register__button__container">
-            <button className="register__button">Create Account</button>
+            <button className="register__button" onClick={handleRegister}>
+              Create Account
+            </button>
           </div>
         </div>
         <div className="register__other__actions">
